f3a781f3ce3008db80dba4788846b345968fe21b
as754770178 <754770178@qq.com>
2020-02-29 13:36:36 +0800

    get_bboxes_single注释生成proposals的流程

--- mmdet/models/anchor_heads/rpn_head.py
+++ mmdet/models/anchor_heads/rpn_head.py
@@ -16,6 +16,7 @@ class RPNHead(AnchorHead):
         super(RPNHead, self).__init__(2, in_channels, **kwargs)
 
     def _init_layers(self):
+        # 构造网络的基础结构
         self.rpn_conv = nn.Conv2d(
             self.in_channels, self.feat_channels, 3, padding=1)
         self.rpn_cls = nn.Conv2d(self.feat_channels,
@@ -28,6 +29,9 @@ class RPNHead(AnchorHead):
         normal_init(self.rpn_reg, std=0.01)
 
     def forward_single(self, x):
+        # RPN网络结构简单
+        # 通道数统一到feat_channels，再扩展到 num_anchors * cls_out_channels 和 num_anchors * 4
+        # 通道数：网格上每个点对应的输出
         x = self.rpn_conv(x)
         x = F.relu(x, inplace=True)
         rpn_cls_score = self.rpn_cls(x)
@@ -67,6 +71,7 @@ class RPNHead(AnchorHead):
             assert rpn_cls_score.size()[-2:] == rpn_bbox_pred.size()[-2:]
             rpn_cls_score = rpn_cls_score.permute(1, 2, 0)
             if self.use_sigmoid_cls:
+                # 单类（只区分前景和背景）用sigmoid操作
                 rpn_cls_score = rpn_cls_score.reshape(-1)
                 scores = rpn_cls_score.sigmoid()
             else:
@@ -74,13 +79,20 @@ class RPNHead(AnchorHead):
                 scores = rpn_cls_score.softmax(dim=1)[:, 1]
             rpn_bbox_pred = rpn_bbox_pred.permute(1, 2, 0).reshape(-1, 4)
             anchors = mlvl_anchors[idx]
+            
+            # 1. 根据feature map中每个格子每个anchor的预测概率值，取出前nms_pre（2000）个
             if cfg.nms_pre > 0 and scores.shape[0] > cfg.nms_pre:
                 _, topk_inds = scores.topk(cfg.nms_pre)
+                # 取出pred系数，网格中anchor，预测概率值
                 rpn_bbox_pred = rpn_bbox_pred[topk_inds, :]
                 anchors = anchors[topk_inds, :]
                 scores = scores[topk_inds]
+            
+            # 根据网格中anchor和pred系数，进行坐标变换生成真实坐标
             proposals = delta2bbox(anchors, rpn_bbox_pred, self.target_means,
                                    self.target_stds, img_shape)
+            
+            # 通过min_bbox_size过滤
             if cfg.min_bbox_size > 0:
                 w = proposals[:, 2] - proposals[:, 0] + 1
                 h = proposals[:, 3] - proposals[:, 1] + 1
@@ -88,10 +100,14 @@ class RPNHead(AnchorHead):
                                            (h >= cfg.min_bbox_size)).squeeze()
                 proposals = proposals[valid_inds, :]
                 scores = scores[valid_inds]
+                
+            # proposals的坐标和对应的置信度拼接在一起，执行nms操作
             proposals = torch.cat([proposals, scores.unsqueeze(-1)], dim=-1)
             proposals, _ = nms(proposals, cfg.nms_thr)
             proposals = proposals[:cfg.nms_post, :]
             mlvl_proposals.append(proposals)
+            
+        # 将所有feature map的proposals拼接在一起 => 所有proposals都是原图尺寸！！！
         proposals = torch.cat(mlvl_proposals, 0)
         if cfg.nms_across_levels:
             proposals, _ = nms(proposals, cfg.nms_thr)
