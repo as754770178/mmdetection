5fbb0d05b197e8d54f547b0ca4203d643507a62f
as754770178 <754770178@qq.com>
2020-03-02 16:13:24 +0800

    multiclass_nms注释

--- mmdet/core/post_processing/bbox_nms.py
+++ mmdet/core/post_processing/bbox_nms.py
@@ -33,19 +33,25 @@ def multiclass_nms(multi_bboxes,
     nms_type = nms_cfg_.pop('type', 'nms')
     nms_op = getattr(nms_wrapper, nms_type)
     for i in range(1, num_classes):
+        # 通过每个bbox在该类置信度过滤
         cls_inds = multi_scores[:, i] > score_thr
         if not cls_inds.any():
             continue
         # get bboxes and scores of this class
+        # 执行过滤
         if multi_bboxes.shape[1] == 4:
             _bboxes = multi_bboxes[cls_inds, :]
         else:
             _bboxes = multi_bboxes[cls_inds, i * 4:(i + 1) * 4]
         _scores = multi_scores[cls_inds, i]
+        
         if score_factors is not None:
             _scores *= score_factors[cls_inds]
+        # 将坐标和类别置信度拼接在一起，执行nms操作
         cls_dets = torch.cat([_bboxes, _scores[:, None]], dim=1)
+        # 获取nms后的bbox
         cls_dets, _ = nms_op(cls_dets, **nms_cfg_)
+        # 获取对应的label
         cls_labels = multi_bboxes.new_full((cls_dets.shape[0], ),
                                            i - 1,
                                            dtype=torch.long)
@@ -62,5 +68,5 @@ def multiclass_nms(multi_bboxes,
     else:
         bboxes = multi_bboxes.new_zeros((0, 5))
         labels = multi_bboxes.new_zeros((0, ), dtype=torch.long)
-
+  
     return bboxes, labels
